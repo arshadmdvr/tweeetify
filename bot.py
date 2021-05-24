3c12783fceb192fa619b82a0ce56562560c5a617
Arshad Madavoor <arshadmdvr@gmail.com>
2021-05-25 01:34:10 +0530

    stored keys as env variables

--- bot.py
+++ bot.py
@@ -3,22 +3,25 @@
 import tweepy
 import logging
 import time
-import config
+import os
+
+consumer_key=os.getenv('ck')
+consumer_secret = os.getenv('cs')
+access_token = os.getenv('atk')
+access_token_secret = os.getenv('ats')
 
 #Setup access to API
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token_key, config.access_token_secret)
-    
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, 
         wait_on_rate_limit_notify=True)
 
-
+#print(consumer_key)
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, s):
-        api.send_direct_message(s.user.id,(api.get_status(s.in_reply_to_status_id)).user.name +" ("+ "@"+ s.in_reply_to_screen_name +") "+ "tweeted: "+ (api.get_status(s.in_reply_to_status_id)).text
-) 
+        api.send_direct_message(s.user.id,(api.get_status(s.in_reply_to_status_id)).user.name +" ("+ "@"+ s.in_reply_to_screen_name +") "+ "tweeted: "+ (api.get_status(s.in_reply_to_status_id)).text) 
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
--- config.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "a75EWKNuDvVihQEy8dE84Z9iO"
-consumer_secret = "UOCqOBKqWHHWcIYO89ZsnCpZcggxfvalZNHeAHEqLgLtaUspha"
-access_token_key = "1395946530805141504-FQPUHrv4YD7rIVGIMemYODpXjstCQC"
-access_token_secret = "0gQznxLCQbL5ANcSwnGetGKx2Bm1ECjEEsXnwRUvHH4zK"
-    
